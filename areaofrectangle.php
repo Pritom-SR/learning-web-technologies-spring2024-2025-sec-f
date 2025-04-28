c51c30f7a32bc6d5ce76c225123e3da85a231efa
Pritom-SR <pritoms010@gmail.com>
2025-04-28 14:44:35 +0600

    length width area perimeter added

---
+++ areaofrectangle.php
@@ -0,0 +1,12 @@
+<?php
+$length = 10; 
+$width = 5;   
+$area = $length * $width;
+
+$perimeter = 2 * ($length + $width);
+
+echo "Length of Rect: " . $length . "<br>";
+echo "Width of Rect: " . $width . "<br>";
+echo "Area of Rect: " . $area . " square<br>";
+echo "Perimeter of Rect: " . $perimeter . "<br>";
+?>
